3902873f05244a5dd4eb44ff5842f172e9f4dfc3
Ulrich Van Den Hekke <ulrich.vdh@shadoware.org>
2018-12-28 23:55:22 +0100

    feat(dns): resolve

--- server/plugins/resolve.ts
+++ server/plugins/resolve.ts
@@ -1,3 +1,29 @@
+import * as dns from 'dns'
+
 export async function resolve (host: string, addresses: Array<string>): Promise<string> {
-  return ''
+  const testableHostname = [host, ...addresses]
+
+  for (let hostname of testableHostname) {
+    const ip = await findIP(hostname)
+    if (ip) {
+      return ip
+    }
+  }
+
+  throw new Error(`Can't find any IP for host ${host}`)
+}
+
+async function findIP (hostname: string): Promise<string> {
+  return resolveDNS(hostname)
+}
+
+async function resolveDNS (hostname: string): Promise<string> {
+  const result = await dns.promises.lookup(hostname)
+  return result.address
+}
+
+async function resolveNetbios (hostname: string): Promise<string> {
+  // exec nmblookup <host>
+  // failed: name_query failed to find name pc-m-ulrich
+  // success: 192.168.176.1 pc-ulrich<00>
 }
