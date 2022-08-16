d3f459f3b45a8dad3aaad3904a12c66d3d87add6
Ulrich Van Den Hekke <ulrich.vdh@shadoware.org>
2022-08-16 16:48:02 +0200

    docs(#38): :memo: Update of the constant

--- nestjs/libs/shared/src/constants/index.ts
+++ nestjs/libs/shared/src/constants/index.ts
@@ -1,7 +1,7 @@
 import * as Long from 'long';
 
 export const HASH_ALGO = 'sha3-256';
-export const CHUNK_SIZE = 1 << 22;
+export const CHUNK_SIZE = 1 << 22; // 4MB // TODO: make this configurable, pass to 16MB
 export const LONG_CHUNK_SIZE = Long.fromNumber(CHUNK_SIZE);
 
 export const WORKER_TYPE = 'worker_type';
