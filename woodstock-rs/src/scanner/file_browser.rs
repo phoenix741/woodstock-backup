f13bb0ede721986d0344bf7ec9bddfcd610631ad
Ulrich Van Den Hekke <ulrich.vdh@shadoware.org>
2024-08-30 18:59:38 +0200

    feat: ✨ parsing on client side will log all error in the journal

--- woodstock-rs/src/scanner/file_browser.rs
+++ woodstock-rs/src/scanner/file_browser.rs
@@ -12,11 +12,10 @@ use futures::pin_mut;
 use futures::stream::StreamExt;
 use futures::Stream;
 use globset::GlobSet;
-use log::error;
+use nix::NixPath;
 use std::collections::HashMap;
+use std::path::Path;
 use std::path::PathBuf;
-use std::{io, path::Path};
-use tokio::fs::DirEntry;
 
 #[cfg(unix)]
 use std::os::unix::fs::MetadataExt;
@@ -31,6 +30,22 @@ use crate::{EntryState, EntryType, FileManifestAcl, FileManifestJournalEntry, Fi
 pub struct CreateManifestOptions {
     pub with_acl: bool,
     pub with_xattr: bool,
+}
+
+struct PathEntryWithError {
+    pub path: PathBuf,
+    pub state: EntryState,
+    pub state_messages: Vec<String>,
+}
+
+impl PathEntryWithError {
+    pub fn with_path(path: &Path) -> Self {
+        Self {
+            path: path.to_path_buf(),
+            state: EntryState::Metadata,
+            state_messages: Vec::new(),
+        }
+    }
 }
 
 /// Checks if a file is authorized based on the include and exclude patterns.
@@ -197,22 +212,31 @@ fn read_acl(file: &Path) -> Result<Vec<FileManifestAcl>> {
 ///
 fn create_manifest_from_file(
     share_path: &Path,
-    path: &Path,
+    entry: PathEntryWithError,
     options: &CreateManifestOptions,
-) -> Result<FileManifestJournalEntry> {
-    let file = share_path.join(path);
+) -> FileManifestJournalEntry {
+    let file = share_path.join(&entry.path);
+    let mut state = entry.state;
+    let mut state_messages = entry.state_messages;
 
     // Check if user has access to the file
-    let metadata = file.symlink_metadata()?;
+    let metadata = file.symlink_metadata();
 
-    let symlink = if metadata.is_symlink() {
-        path_to_vec(file.read_link().unwrap_or_default().as_path())
-    } else {
-        Vec::new()
+    let (symlink, stats) = match metadata {
+        Ok(metadata) => {
+            let symlink = if metadata.is_symlink() {
+                path_to_vec(file.read_link().unwrap_or_default().as_path())
+            } else {
+                Vec::new()
+            };
+            (symlink, Some(create_stats_from_metadata(&metadata)))
+        }
+        Err(e) => {
+            state = EntryState::Error;
+            state_messages.push(format!("{:#}", e));
+            (Vec::new(), None)
+        }
     };
-
-    let mut state = EntryState::Metadata;
-    let mut state_messages: Vec<String> = Vec::new();
 
     let xattr = if options.with_xattr {
         match read_xattr(&file) {
@@ -240,11 +264,11 @@ fn create_manifest_from_file(
         Vec::new()
     };
 
-    Ok(FileManifestJournalEntry {
+    FileManifestJournalEntry {
         r#type: EntryType::Add as i32,
         manifest: Some(FileManifest {
-            path: path_to_vec(path),
-            stats: Some(create_stats_from_metadata(&metadata)),
+            path: path_to_vec(&entry.path),
+            stats,
 
             xattr,
             acl,
@@ -257,63 +281,93 @@ fn create_manifest_from_file(
 
         state: state as i32,
         state_messages,
-    })
+    }
 }
 
-// TODO: If a directory is not authorized, we should provide an error instead of
-// silently ignoring it.
+async fn one_level(
+    path: &PathBuf,
+    to_visit: &mut Vec<PathBuf>,
+
+    share_path: &Path,
+    includes: &GlobSet,
+    excludes: &GlobSet,
+) -> Vec<PathEntryWithError> {
+    if !path.is_empty() && !is_file_authorized(path, includes, excludes) {
+        return Vec::new();
+    }
+
+    let mut entry = PathEntryWithError::with_path(path);
+    let mut dir = match tokio::fs::read_dir(&share_path.join(path)).await {
+        Ok(dir) => dir,
+        Err(e) => {
+            entry.state = EntryState::Error;
+            entry.state_messages.push(format!("{:#}", e));
+            return vec![entry];
+        }
+    };
+
+    let mut files = Vec::new();
+
+    loop {
+        let child = dir.next_entry().await;
+
+        // En cas d'erreur, on log
+        let child = match child {
+            Ok(child) => child,
+            Err(e) => {
+                entry.state = EntryState::Error;
+                entry.state_messages.push(format!("{:#}", e));
+                break;
+            }
+        };
+        // Si vide on continue
+        let Some(child) = child else {
+            break;
+        };
+
+        let child_path = path.join(child.file_name());
+        let mut child_entry = PathEntryWithError::with_path(&child_path);
+
+        let metadata = match child.metadata().await {
+            Ok(metadata) => metadata,
+            Err(e) => {
+                child_entry.state = EntryState::Error;
+                child_entry.state_messages.push(format!("{:#}", e));
+                files.push(child_entry);
+                continue;
+            }
+        };
+
+        // Si c'est un dossier, on ajoute à la liste des dossiers à visiter
+        if metadata.is_dir() {
+            to_visit.push(child_path);
+        } else if is_file_authorized(&child_path, includes, excludes) {
+            files.push(child_entry);
+        }
+    }
+
+    files.push(entry);
+
+    files
+}
+
 fn get_files_recursive(
     share_path: &Path,
     includes: &GlobSet,
     excludes: &GlobSet,
-) -> impl Stream<Item = DirEntry> + Send + 'static {
-    async fn one_level(
-        path: PathBuf,
-        to_visit: &mut Vec<PathBuf>,
-        share_path: &PathBuf,
-        includes: &GlobSet,
-        excludes: &GlobSet,
-    ) -> io::Result<Vec<DirEntry>> {
-        let reduced_path = path.strip_prefix(share_path).unwrap();
-        if !is_file_authorized(reduced_path, includes, excludes) {
-            return Ok(Vec::new());
-        }
-
-        let mut dir = tokio::fs::read_dir(&path).await?;
-        let mut files = Vec::new();
-
-        while let Some(child) = dir.next_entry().await? {
-            if child.metadata().await?.is_dir() {
-                to_visit.push(child.path());
-            }
-
-            let file = reduced_path.join(child.file_name());
-            if is_file_authorized(&file, includes, excludes) {
-                files.push(child);
-            }
-        }
-
-        Ok(files)
-    }
-
+) -> impl Stream<Item = PathEntryWithError> + Send + 'static {
     let share_path = share_path.to_path_buf();
     let includes = includes.clone();
     let excludes = excludes.clone();
 
     futures::stream::unfold(
-        (vec![share_path.clone()], share_path, includes, excludes),
+        (vec![PathBuf::from("")], share_path, includes, excludes),
         |(mut to_visit, share_path, includes, excludes)| async {
             let path: PathBuf = to_visit.pop()?;
 
             let file_stream =
-                match one_level(path, &mut to_visit, &share_path, &includes, &excludes).await {
-                    Ok(files) => futures::stream::iter(files).left_stream(),
-                    Err(e) => futures::stream::iter({
-                        error!("Can't read the file in directory: {}", e);
-                        Vec::<DirEntry>::new()
-                    })
-                    .right_stream(),
-                };
+                one_level(&path, &mut to_visit, &share_path, &includes, &excludes).await;
+            let file_stream = futures::stream::iter(file_stream);
 
             Some((file_stream, (to_visit, share_path, includes, excludes)))
         },
@@ -346,24 +400,45 @@ pub fn get_files<'a>(
         loop {
             match files.next().await {
                 Some(entry) => {
-                    // Transform entry.path() to be relative to share_path
-                    let entry_path = entry.path();
-                    let path = entry_path.strip_prefix(share_path).unwrap();
-
-                    let manifest = create_manifest_from_file(share_path, &path, options);
-                    match manifest {
-                        Ok(manifest) => yield manifest,
-                        Err(e) => {
-                            error!(
-                                "Can't read the file {}: {}",
-                                path.to_str().unwrap_or_default(),
-                                e
-                            );
-                        }
-                    }
+                    yield create_manifest_from_file(share_path, entry, options);
                 }
                 None => break,
             }
         }
     })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::utils::path::{list_to_globset, vec_to_path};
+
+    #[tokio::test]
+    async fn test_get_files() {
+        let dir_path = Path::new("./data");
+
+        let includes = list_to_globset(&["*.pem"]).unwrap();
+        let excludes = list_to_globset(&[]).unwrap();
+
+        let options = CreateManifestOptions {
+            with_acl: false,
+            with_xattr: false,
+        };
+
+        let stream = get_files(dir_path, &includes, &excludes, &options);
+        pin_mut!(stream);
+
+        let mut filenames = Vec::new();
+
+        while let Some(entry) = stream.next().await {
+            let path = vec_to_path(&entry.manifest.unwrap().path);
+            println!("{:?} {:?} {:?}", path, entry.state, entry.state_messages);
+            filenames.push(path);
+        }
+
+        assert!(filenames.len() >= 3);
+        assert!(filenames.contains(&PathBuf::from("")));
+        assert!(filenames.contains(&PathBuf::from("private_key.pem")));
+        assert!(filenames.contains(&PathBuf::from("public_key.pem")));
+    }
 }
