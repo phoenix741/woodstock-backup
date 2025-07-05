6da21dfc8fccd4e8b4f72380fde71e6730d10eb6
Ulrich Van Den Hekke <ulrich.vdh@shadoware.org>
2025-07-05 09:01:20 +0200

    build: 👷 fix update of version of cargo file

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -2,4 +2,4 @@
 VERSION=$(jq -r .version package.json)
 echo "Preparing release $VERSION"
 npm version $VERSION --allow-same-version --no-git-tag-version --workspaces
-cargo workspaces version -a --force * --no-git-commit -y custom $VERSION
+cargo workspaces version -a --force '*' --no-git-commit -y custom $VERSION
